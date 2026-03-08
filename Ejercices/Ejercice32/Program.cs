db675048a9c6e3652df7897aef5298129eca0045
user <user@DESKTOP-PNR9EFR>
2026-03-08 09:25:40 -0500

    Add Ejercice 32

---
+++ Ejercices/Ejercice32/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Ejercice32
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Ingrese el tamaño del rombo: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        char[,] r = new char[n, n];
+
+        // Inicializar la matriz con espacios en blanco
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                r[i, j] = ' ';
+            }
+        }
+
+        int m = n / 2;
+
+        // Dibujar el rombo
+        for (int i = 0; i <= m; i++)
+        {
+            r[i, m - i] = '#';
+            r[i, m + i] = '#';
+            r[n - i - 1, m - i] = '#';
+            r[n - i - 1, m + i] = '#';
+        }
+
+        // Mostrar el rombo
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(r[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
